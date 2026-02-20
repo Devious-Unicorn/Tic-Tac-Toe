09bd512c58af2a403b43b242d7d4b4bae0d8ef99
Glenn Lawrence <gslawrence27@gmail.com>
2026-02-19 17:06:55 -0700

    Create game scene

--- Assets/Scripts/main.gd
+++ Assets/Scripts/main.gd
@@ -1,12 +1,18 @@
 extends Control
 
 @onready var packedScenes := {
-	"mainMenu" = preload("res://Assets/Scenes/main_menu.tscn")
+	"mainMenu" = preload("res://Assets/Scenes/main_menu.tscn"),
+	"game" = preload("res://Assets/Scenes/game.tscn")
 }
 @onready var scenes := {
-	"mainMenu" = null
+	"mainMenu" = null,
+	"game" = null
 }
 
 func _ready() -> void:
 	scenes.mainMenu = packedScenes.mainMenu.instantiate()
 	add_child(scenes.mainMenu)
+
+func play():
+	scenes.game = packedScenes.game.instantiate()
+	add_child(scenes.game)
